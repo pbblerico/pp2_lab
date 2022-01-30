5e5c224d661b04263c2a8cc3a88975fe27a47257
21B030824 <98637349+21B030824@users.noreply.github.com>
2022-01-30 09:30:29 +0600

    com

--- lab1/i.py
+++ lab1/i.py
@@ -1,7 +1,17 @@
-a, b = [input() for i in range(int(input()))], '@gmail.com'
-out = []
-for i in range(len(a)):
-    if b in a[i]:
-        c = len(a[i]) - len(b)
-        out.append(a[i][:c])
-print(*out, sep='/n')
+#a, b = [input() for i in range(int(input()))], '@gmail.com'
+#out = []
+#for i in range(len(a)):
+#    if b in a[i]:
+#       c = len(a[i]) - len(b)
+#        out.append(a[i][:c])
+##if len(out) != 0:
+#    print(*out, sep='/n')
+
+b = '@gmail.com'
+for i in range(int(input())):
+    s = ''
+    a = input()
+    if b in a:
+        s = a[:len(a) - len(b)]
+    if len(s) != 0:
+        print(s)
